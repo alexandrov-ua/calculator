f3384a4cf7a03cb678f7b0fe50a2b6ef5e73ae22
anders_ua <alexandrov.ua@gmail.com>
2019-03-16 15:51:56 +0200

    Init: lexer added

---
+++ Calculator.Common/SyntaxTokenKind.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator.Common
+{
+    public enum SyntaxTokenKind
+    {
+        Plus,
+        Minus,
+        Star,
+        Slash,
+        OpenParenthesis,
+        CloseParenthesis,
+        Number,
+        Identifier,
+        Unknown
+    }
+}
