e3ae44330bf34cc57f87e9e765856c69c42826ad
alexandrov-ua <alexandrov.ua+1@gmail.com>
2019-03-18 20:53:36 +0200

    FE: REPL-like behavioure + syles

--- Calculator.Api/Scripts/App/main.js
+++ Calculator.Api/Scripts/App/main.js
@@ -1,5 +1,30 @@
 ﻿window.onload = function () {
-    
+    ko.extenders.scrollFollow = function (target, selector) {
+        target.subscribe(function (newval) {
+            var el = document.querySelector(selector);
+
+            if (el.scrollTop == el.scrollHeight - el.clientHeight) {
+                setTimeout(function () { el.scrollTop = el.scrollHeight - el.clientHeight; }, 0);
+            }
+        });
+
+        return target;
+    };
+
+    ko.bindingHandlers.enterkey = {
+        init: function (element, valueAccessor, allBindings, viewModel) {
+            var callback = valueAccessor();
+            $(element).keypress(function (event) {
+                var keyCode = (event.which ? event.which : event.keyCode);
+                if (keyCode === 13) {
+                    callback.call(viewModel);
+                    return false;
+                }
+                return true;
+            });
+        }
+    };
+
     $.ajaxSetup({
         beforeSend: function (xhr) {
             xhr.setRequestHeader("Accept", "application/json");
@@ -15,10 +40,10 @@
     function AppViewModel() {
         this.input = ko.observable("asd");
         this.output = ko.observable("");
-        this.items = ko.observableArray([]);
+        this.items = ko.observableArray([]).extend({ scrollFollow: '#container' });
         this.calculate = function () {
             var self = this;
-            this.items.push(new Item(this.input()));
+            this.items.push(new Item(">" + this.input()));
             $.post("api/evaluator/evaluate",
                 JSON.stringify({ expression: this.input() }),
                 function (response) {
@@ -27,17 +52,13 @@
                 });
 
             this.output();
-            var rowpos = $('#container tr:last').position();
-            console.log(rowpos.top);
-            $('#container').scrollTop(rowpos.top);
         };
 
-        this.keypressed = function (data, event) {
-            if (event.which == 13) {
-                this.calculate();
-            }
+        this.onEnterKey = function () {
+            this.calculate();
         }
     }
     ko.applyBindings(new AppViewModel());
+    $("#input").focus();
 }
 
